483461d48aa27482b33b97abb5399e440cbe1164
unknown <Stefan.Carlton@VESLPTP6582.wilson.com>
2015-02-20 16:18:38 +1100

    convert to bash

--- minify.sh
+++ minify.sh
@@ -1,4 +1,4 @@
-shopt -s extglob
+#!/bin/bash
 
 # Remove unused kernels
 apt-get purge -y $(dpkg -l 'linux-image-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
